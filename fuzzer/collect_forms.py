a5cd166cd5a4e4bd65c4b44ccb42413226dabb69
hadiMh <hadih7899@gmail.com>
2021-06-19 03:35:13 +0430

    Use helper.py functions from collect_forms

--- fuzzer/collect_forms.py
+++ fuzzer/collect_forms.py
@@ -3,37 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from html_classes import Form, Input, WebPage
-
-def setup_driver():
-    """
-    Create a driver and Returns it.
-    When someone runs this python file this function helps them to have a new driver.
-    """
-
-    # with these settings, loading of a webpage would be faster.
-    # because selenium won't wait for images to load and we don't need images.
-    caps = DesiredCapabilities().CHROME
-    caps["pageLoadStrategy"] = "eager"
-
-    # creating driver.
-    driver = webdriver.Chrome(desired_capabilities=caps)
-
-    return driver
-
-
-def get_all_urls_of_file():
-    """
-    Gets all the urls from the previous phases and creates a list of them.
-    Return this list of all the urls that have been found on last phases.
-    """
-    all_found_urls = []
-    with open('all_explored_urls.txt', 'r') as file:
-        all_found_urls = file.readlines()
-
-        all_found_urls = list(
-            map(lambda url: url[:-1] if url[-1:] == '\n' else url, all_found_urls))
-
-    return all_found_urls
+from helpers import setup_driver, get_all_urls_of_file
 
 
 def get_forms_of_all_pages_to_objs(all_urls, driver):
@@ -47,7 +17,6 @@ def get_forms_of_all_pages_to_objs(all_urls, driver):
         all_web_pages_objs.append(WebPage(url, driver=driver))
 
     return all_web_pages_objs
-
 
 
 if __name__ == '__main__':
